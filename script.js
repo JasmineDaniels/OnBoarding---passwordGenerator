1eaea5596ee7cf533796169e05aa488620c4c39e
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-05-13 10:42:54 -0400

    Return value generate

--- script.js
+++ script.js
@@ -13,5 +13,22 @@ function writePassword() {
 
 }
 
+
+function generatePassword () {
+
+  var disclosure = confirm('Begin Password Generator?')
+  var lengthChar = prompt('How many Characters?', 'Enter Password Length')
+  var upperChar =  confirm('Include Uppercase?', 'Enter Characters')
+  var lowerChar=  confirm('Include Lowercase?', 'Enter Characters')
+  var numChar =  confirm('Include Numbers?', 'Enter Characters')
+  var specChar =  confirm('Include Special Characters?', 'Enter Characters')
+  var createdAlert = alert('Password has been Created')
+
+
+  generate = disclosure + lengthChar + upperChar + lowerChar + numChar + specChar + createdAlert
+  return generate
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
