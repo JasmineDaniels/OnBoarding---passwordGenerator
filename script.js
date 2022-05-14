22b6e9fa590bd4895eed5e786dd2241341833365
Jasmine Daniels <create.jasminedaniels@gmail.com>
2022-05-14 00:20:29 -0400

    [spec chars spread] + Math.random

--- script.js
+++ script.js
@@ -14,21 +14,53 @@ function writePassword() {
 }
 
 
-function generatePassword () {
+var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var spec = ['&', '!', '#', '%', '*', '$', '(', ')', '+', '-', ',', '.', '\', '/', \':', ';', '<', '=', '>', '?', '@', '[', '\', \']', '^', '_', '`', '{', '|', '}', '~']
+var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var upper =['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
+
+function generatePassword () {
+  var possible = []
   var disclosure = confirm('Begin Password Generator?')
   var lengthChar = prompt('How many Characters?', 'Enter Password Length')
-  var upperChar =  confirm('Include Uppercase?', 'Enter Characters')
-  var lowerChar=  confirm('Include Lowercase?', 'Enter Characters')
-  var numChar =  confirm('Include Numbers?', 'Enter Characters')
-  var specChar =  confirm('Include Special Characters?', 'Enter Characters')
-  var createdAlert = alert('Password has been Created')
+  if (lengthChar >= 8 && lengthChar <= 128) {
+    var upperChar =  confirm('Include Uppercase?', 'Enter Characters')
+    var lowerChar=  confirm('Include Lowercase?', 'Enter Characters')
+    var numChar =  confirm('Include Numbers?', 'Enter Characters')
+    var specChar =  confirm('Include Special Characters?', 'Enter Characters')
+    var createdAlert = alert('Password has been Created')
+
+    if (upperChar) {
+      possible = [...upper, ...possible]
+    } if (lowerChar) {
+      possible = [...lower, ...possible]
+    } if (numChar) {
+      possible = [...nums, ...possible]
+    } if (specChar) {
+      possible = [...spec, ...possible]
+    }
+
+    console.log(possible)
+    // Function 
+
+    
+    var test = " "
+    var str = '';
+    for (i=0; i <= lengthChar.length; i++) {
+      
+      var str = possible[Math.floor(Math.random()*[lengthChar.length])]
+      test += str[possible]
+      console.log(test)
+    }
 
 
-  generate = disclosure + lengthChar + upperChar + lowerChar + numChar + specChar + createdAlert
-  return generate
+    //  return password
+  } else {
+    generatePassword ()
+  }
+
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
